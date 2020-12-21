762db68cbc40b1adc3a6d22c1cdbde917057cf61
Roman Kochanov <lineblaze@mail.ru>
2020-12-21 18:44:35 +0300

    4

--- script.js
+++ script.js
@@ -31,6 +31,15 @@ console.log('\n')
 console.log('Задание 3, Конвертация единиц измерения');
 let c = 25;
 let f = 87;
-console.log(`${c} C\u{00B0} = ${c * 1.8 + 32} F\u{00B0}`);
-console.log(`${f} F\u{00B0} = ${(f - 32) / 1.8} C\u{00B0}`);
+console.log(`${c} C\u{00B0} соответствует ${c * 1.8 + 32} F\u{00B0}`);
+console.log(`${f} F\u{00B0} соответствует ${(f - 32) / 1.8} C\u{00B0}`);
+console.log('\n')
+
+/* Задание 4 */
+console.log('Задание 4, Високосный год');
+let year;
+let vis;
+year = prompt("Какой сейчас год?", "")
+vis = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? true : false;
+console.log(`${year}: ${vis}`)
 console.log('\n')
