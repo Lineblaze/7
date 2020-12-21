5676c1db1fdf26095268c26d672158641e9a4099
Roman Kochanov <lineblaze@mail.ru>
2020-12-21 18:37:20 +0300

    2

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ console.log(`number "${num}" + boolean "${bool}" = "${num + bool}"`); // = numbe
 console.log(`number "${num}" + null "${prim}" = "${num + prim}"`); // = number
 console.log(`boolean "${bool}" + null "${prim}" = "${bool + prim}"`); // = number
 console.log('\n')
+
+/* Задание 2 */
+console.log('Задание 2, Прямоугольник');
+const a = 17;		// Сторона a
+const b = 13;		// Сторона b
+let ar, per;		// Площадь и периметр
+console.log(`a = ${a}, b = ${b}`);
+console.log(`Площадь = ${a * b}`);
+console.log(`Периметр = ${(a + b) * 2}`);
+console.log(`Отношение площади к периметру = ${a * b / ((a + b) * 2)}`);
+console.log('\n')
